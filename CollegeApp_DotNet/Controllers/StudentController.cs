dab2a0284d1ef8806bc1174de16fd20a68c67122
Pranava <th4274@365sg.xyz>
2021-12-01 23:08:02 +0530

    Added Serilog and a few logs

--- CollegeApp_DotNet/Controllers/StudentController.cs
+++ CollegeApp_DotNet/Controllers/StudentController.cs
@@ -2,8 +2,6 @@
 using CollegeApp_DotNet.BusinessDomain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Threading.Tasks;
 
 namespace CollegeApp_DotNet.WebServices.Controllers
 {
@@ -12,10 +10,12 @@ namespace CollegeApp_DotNet.WebServices.Controllers
     public class StudentController : ControllerBase
     {
         private readonly IStudentBL studentBL;
+        private readonly Serilog.ILogger logger;
 
-        public StudentController(IStudentBL studentBL)
+        public StudentController(IStudentBL studentBL, Serilog.ILogger logger)
         {
             this.studentBL = studentBL;
+            this.logger = logger;
         }
 
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -24,13 +24,16 @@ namespace CollegeApp_DotNet.WebServices.Controllers
         [HttpGet("{departmentUid}")]
         public IActionResult GetAllStudents(string departmentUid)
         {
+            logger.Information("Module: StudentController/GetAllStudents - API : START");
             ResponseMessage<List<StudentDepartmentDetails>> response = this.studentBL.GetStudentDetails(departmentUid);
             if(response.IsSuccess == false)
             {
+                logger.Information("Module: StudentController/GetAllStudents - API : END");
                 return NotFound(response);
             }
             else
             {
+                logger.Information("Module: StudentController/GetAllStudents - API : END");
                 return Ok(response);
             }
         }
--- CollegeApp_DotNet/Program.cs
+++ CollegeApp_DotNet/Program.cs
@@ -1,9 +1,18 @@
 using CollegeApp_DotNet.DataAccess.Models;
 using CollegeApp_DotNet.WebServices;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Templates;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.WriteTo.File("./logs/error/logError_.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, restrictedToMinimumLevel: LogEventLevel.Error);
+    lc.WriteTo.File("./logs/info/logInformation_.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, restrictedToMinimumLevel: LogEventLevel.Information);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
