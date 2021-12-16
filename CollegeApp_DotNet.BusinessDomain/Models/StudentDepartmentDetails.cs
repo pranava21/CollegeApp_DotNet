207bb49de1d86272c1c0cc6316214514bc12af1b
Pranava <th4274@365sg.xyz>
2021-12-16 21:02:44 +0530

    added appSettings.PROD.json

--- CollegeApp_DotNet.BusinessDomain/Models/StudentDepartmentDetails.cs
+++ CollegeApp_DotNet.BusinessDomain/Models/StudentDepartmentDetails.cs
@@ -8,11 +8,11 @@ namespace CollegeApp_DotNet.BusinessDomain.Models
 {
     public class StudentDepartmentDetails
     {
-        public string StudentUid { get; set; }
-        public string StudentName { get; set;}
-        public string StudentEmail { get; set;}
-        public string PhoneNo { get; set;}
-        public string DepartmentUid { get; set; }
-        public string DepartmentName { get; set; }
+        public string? StudentUid { get; set; }
+        public string? StudentName { get; set;}
+        public string? StudentEmail { get; set;}
+        public string? PhoneNo { get; set;}
+        public string? DepartmentUid { get; set; }
+        public string? DepartmentName { get; set; }
     }
 }
